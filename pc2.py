0fe3926ec54575fcfefab2ab9ce7061b70fa61bc
Jorge <jorge.antolin5232@alumnos.udg.mx>
2023-04-27 01:00:56 -0600

    Login/register/verify

---
+++ pc2.py
@@ -0,0 +1,174 @@
+from ast                import If
+from threading          import activeCount
+from time               import time
+from flask              import Flask, render_template, request, redirect, url_for, session, flash, make_response
+from flask_mysqldb      import MySQL, MySQLdb
+from flask_mail         import Mail, Message
+from flask_bcrypt       import bcrypt,Bcrypt
+from flask_login        import LoginManager, login_user, logout_user, login_required, login_manager
+from flask_wtf.csrf     import CSRFProtect
+from functools          import wraps
+from werkzeug.utils     import secure_filename
+from datetime           import date, datetime
+import base64
+import pdfkit
+import os
+import random
+import secrets
+
+
+PCapp                                   = Flask(__name__)
+mysql                                   = MySQL(PCapp)
+csrf=CSRFProtect()
+PCapp.config['MYSQL_HOST']              = 'localhost'
+PCapp.config['MYSQL_USER']              = 'root'
+PCapp.config['MYSQL_PASSWORD']          = 'antolin1'
+PCapp.config['MYSQL_DB']                = 'rm'
+PCapp.config['MYSQL_CURSORCLASS']       = 'DictCursor'
+PCapp.config['UPLOAD_FOLDER']           = './static/img/'
+PCapp.config['UPLOAD_FOLDER_PDF']       = './static/pdf/'
+
+
+PCapp.config['MAIL_SERVER']='smtp.gmail.com'
+PCapp.config['MAIL_PORT'] = 465
+PCapp.config['MAIL_USERNAME'] = 'jsecardenas120@gmail.com'
+PCapp.config['MAIL_PASSWORD'] = 'gfpsmrtwpqwrafqz'
+PCapp.config['MAIL_USE_TLS'] = False
+PCapp.config['MAIL_USE_SSL'] = True
+mail = Mail(PCapp)
+
+bcryptObj = Bcrypt(PCapp)
+
+
+
+#El Def auth controla si inicia sesion o es nuevo usuario
+
+@PCapp.route('/pythonlogin/', methods=['GET', 'POST'])
+def auth():
+    if request.method == 'POST':
+        action = request.form['action']
+        if action == 'login':
+            email = request.form['email']
+            password = request.form['password'].encode('utf-8')
+            cur = mysql.connection.cursor()
+            result = cur.execute("SELECT * FROM user WHERE correoAd=%s ", [email])
+            if result > 0:
+                data = cur.fetchone()
+                if bcrypt.checkpw(password, data['passAd'].encode('utf-8')):
+                    if data['verificado'] == 1:
+                        #Falta agregar sessions ( los adecuados )
+                        
+                        session["login"] = True
+                        session['name'] = data['nombreAd']
+                        flash("Inicio de Sesión exitoso", 'success')
+                        cur.close()
+                        return redirect(url_for('home'))
+                    else:
+                        # El usuario no está verificado, mostrar mensaje de error
+                        flash("Debes verificar tu cuenta antes de iniciar sesión", 'warning')
+                else:
+                    flash("Contraseña incorrecta", 'danger')
+            else:
+                flash("Email no registrado", 'danger')
+                
+        #Registro
+        elif action == 'register':
+            #Falta Hacer HASH a toda la informacion personal del usuario
+            name = request.form['name']
+            email = request.form['email']
+            tel = request.form['phone']
+            password = request.form['password']
+            hashed_password = bcryptObj.generate_password_hash(password).decode('utf-8')
+            
+            # Verificar si el correo ya está registrado en la base de datos
+            cur = mysql.connection.cursor()
+            result = cur.execute("SELECT * FROM user WHERE correoAd=%s", [email])
+            if result > 0:
+                # Si el correo ya está registrado, mostrar un mensaje de error
+                flash("El correo ya está registrado", 'danger')
+                cur.close()
+                return redirect(url_for('auth'))
+            
+            # Generar un código de verificación aleatorio
+            verification_code = secrets.token_hex(3)
+            cur = mysql.connection.cursor()
+            
+            # Guardar el usuario y el código de verificación en la base de datos
+            cur.execute("INSERT INTO user(nombreAd, correoAd, telCelAd, passAd, verification_code) VALUES(%s, %s, %s, %s, %s)", (name, email, tel, hashed_password, verification_code))
+            mysql.connection.commit()
+            cur.close()
+            
+            # Enviar el código de verificación por correo electrónico
+            msg = Message('Código de verificación', sender='jsecardenas120@gmail.com', recipients=[email])
+            msg.body = f"¡Bienvenido! Gracias por registrarte a PSICONNECTION.\n\nTu código de verificación es: {verification_code}\n\n¿No has sido tú? No te preocupes, simplemente haz caso omiso a este correo.\n"
+            mail.send(msg)
+            
+            # Redirigir al usuario a la página de verificación
+            flash("Revisa tu correo electrónico para obtener tu código de verificación", 'success')
+            return redirect(url_for('verify'))
+
+    return render_template('login.html')
+    
+
+
+@PCapp.route('/verify', methods=['GET', 'POST'])
+def verify():
+    if request.method == 'POST':
+        # Obtener el código ingresado por el usuario
+        user_code = request.form['code']
+        
+        # Verificar si el código es correcto
+        cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * FROM user WHERE verification_code=%s", [user_code])
+        if result > 0:
+            # Si el código es correcto, actualizar el campo "verificado" a 1
+            cur.execute("UPDATE user SET verificado = %s WHERE verification_code = %s", (1, user_code))
+            mysql.connection.commit()
+            flash("Registro completado con éxito", 'success')
+            cur.close()
+            return redirect(url_for('home'))
+        else:
+            flash("Código de verificación incorrecto", 'danger')
+            cur.close()
+        
+    return render_template('verify.html')
+
+
+
+@PCapp.route('/')
+def home():
+    # Check if user is loggedin
+    if 'login' in session:
+        # User is loggedin show them the home page
+        return render_template('index.html', username=session['name'])
+    
+    # User is not loggedin redirect to login page
+    return redirect(url_for('auth'))
+
+
+@PCapp.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('login', None)
+   session.pop('userid', None)
+   session.pop('email', None)
+   # Redirect to login page
+   return redirect(url_for('auth'))
+
+@PCapp.route('/protected')
+@login_required
+def protected ():
+    return "<h1>Esta es una vista protegida, solo para usuarios autenticados.</h1>"
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+def status_404(error):
+    return "<h1>Pagina no encontrada </h1>", 404
+
+if __name__ == '__main__':
+    PCapp.secret_key = '123'
+    csrf.init_app(PCapp)
+    PCapp.register_error_handler(401,status_401)
+    PCapp.register_error_handler(404,status_404)
+    PCapp.run(port=3000,debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,151 @@
+/*global $, document, window, setTimeout, navigator, console, location*/
+$(document).ready(function () {
+
+    'use strict';
+
+    var usernameError = true,
+        emailError    = true,
+        passwordError = true,
+        passConfirm   = true;
+
+    // Detect browser for css purpose
+    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        $('.form form label').addClass('fontSwitch');
+    }
+
+    // Label effect
+    $('input').focus(function () {
+
+        $(this).siblings('label').addClass('active');
+    });
+
+    // Form validation
+    $('input').blur(function () {
+
+        // User Name
+        if ($(this).hasClass('name')) {
+            if ($(this).val().length === 0) {
+                $(this).siblings('span.error').text('Porfavor ingrese su nombre completo').fadeIn().parent('.form-group').addClass('hasError');
+                usernameError = true;
+            } else if ($(this).val().length > 1 && $(this).val().length <= 6) {
+                $(this).siblings('span.error').text('Porfavor ingrese al menos 6 caracteres').fadeIn().parent('.form-group').addClass('hasError');
+                usernameError = true;
+            } else {
+                $(this).siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+                usernameError = false;
+            }
+        }
+        // Email
+        if ($(this).hasClass('email')) {
+            if ($(this).val().length == '') {
+                $(this).siblings('span.error').text('Porfavor ingrese su correo electronico').fadeIn().parent('.form-group').addClass('hasError');
+                emailError = true;
+            } else {
+                $(this).siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+                emailError = false;
+            }
+        }
+
+        // PassWord
+        if ($(this).hasClass('pass')) {
+            if ($(this).val().length < 8) {
+                $(this).siblings('span.error').text('Porfavor ingrese al menos 8 caracteres').fadeIn().parent('.form-group').addClass('hasError');
+                passwordError = true;
+            } else {
+                $(this).siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+                passwordError = false;
+            }
+        }
+
+        // PassWord confirmation
+        if ($('.pass').val() !== $('.passConfirm').val()) {
+            $('.passConfirm').siblings('.error').text('Las contraseñas no coinciden').fadeIn().parent('.form-group').addClass('hasError');
+            passConfirm = false;
+        } else {
+            $('.passConfirm').siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+            passConfirm = false;
+        }
+
+        // label effect
+        if ($(this).val().length > 0) {
+            $(this).siblings('label').addClass('active');
+        } else {
+            $(this).siblings('label').removeClass('active');
+        }
+    });
+
+
+    // form switch
+    $('a.switch').click(function (e) {
+        $(this).toggleClass('active');
+        e.preventDefault();
+
+        if ($('a.switch').hasClass('active')) {
+            $(this).parents('.form-peice').addClass('switched').siblings('.form-peice').removeClass('switched');
+        } else {
+            $(this).parents('.form-peice').removeClass('switched').siblings('.form-peice').addClass('switched');
+        }
+    });
+
+
+    $(document).ready(function() {
+        // Mostrar las alertas con animación
+        $('.flashes li').each(function() {
+          $(this).addClass('show');
+        });
+      
+        // Ocultar las alertas con animación después de 5 segundos
+        setTimeout(function() {
+          $('.flashes li').removeClass('show');
+          setTimeout(function() {
+            $('.flashes li').remove();
+          }, 500);
+        }, 5000);
+      });
+
+
+
+
+
+});
+
+
+/* Form submit
+$('form.signup-form').submit(function (event) {
+    event.preventDefault();
+
+    if (usernameError == true || emailError == true || passwordError == true || passConfirm == true) {
+        $('.name, .email, .pass, .passConfirm').blur();
+    } else {
+        // Obtener los datos del formulario
+        var formData = {
+            'email': $('input[name=email]').val(),
+            'password': $('input[name=password]').val(),
+            'action': 'register'
+        };
+        
+        // Enviar los datos del formulario a la ruta /pythonlogin
+        $.ajax({
+            type: 'POST',
+            url: '/pythonlogin',
+            data: formData,
+            dataType: 'json',
+            encode: true
+        }).done(function(data) {
+            // Manejar la respuesta del servidor
+            // ...
+        });
+        
+            $('.signup, .login').addClass('switched');
+
+            setTimeout(function () { $('.signup, .login').hide(); }, 700);
+            setTimeout(function () { $('.brand').addClass('active'); }, 300);
+            setTimeout(function () { $('.heading').addClass('active'); }, 600);
+            setTimeout(function () { $('.success-msg p').addClass('active'); }, 900);
+            setTimeout(function () { $('.success-msg a').addClass('active'); }, 1050);
+            setTimeout(function () { $('.form').hide(); }, 700);
+        });
+    }
+});
+Mensaje recibido.
+*/
