7d41ee9f38f3c99abf82fe777064eccee27ba468
Ricardo1232 <ricardo.velazquez5779@alumnos.udg.mx>
2023-05-08 12:15:49 -0600

    Pacientes

---
+++ static/js/jspersonal.js
@@ -0,0 +1,20 @@
+
+// const bg = document.getElementById('prueba');
+
+
+// const positions = [
+//   'top left',
+//   'top right',
+//   'bottom left',
+//   'bottom right',
+//   'center'
+// ];
+
+// function setBackground() {
+//   const randomImage = Math.floor(Math.random() * images.length);
+//   const randomPosition = positions[Math.floor(Math.random() * positions.length)];
+//   bg.style.backgroundImage = images[randomImage];
+//   bg.style.backgroundPosition = randomPosition;
+// }
+
+// setBackground();
