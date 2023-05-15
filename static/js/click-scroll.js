0b50cb38e1e73a43c86459b988cb52149ffb2979
Ricardo1232 <ricardo.velazquez5779@alumnos.udg.mx>
2023-05-14 18:09:10 -0600

    Paciente - Final - Detalles

--- static/js/click-scroll.js
+++ static/js/click-scroll.js
@@ -6,7 +6,11 @@ var sectionArray = [1, 2, 3, 4, 5];
 $.each(sectionArray, function(index, value){
           
      $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 90;
+        // var offsetSection = $('#' + 'section_' + value).offset().top - 90;
+        var section = $('#' + 'section_' + value);
+        if (section.length > 0) {
+            var offsetSection = section.offset().top - 90;
+        }
          var docScroll = $(document).scrollTop();
          var docScroll1 = docScroll + 1;
          
--- static/js/jspersonal.js
+++ static/js/jspersonal.js
@@ -6,65 +6,355 @@ var navLinks = document.querySelectorAll('.nav-link.click-scroll');
 // Iteramos por cada enlace y comparamos su URL con la URL actual de la página
 for (var i = 0; i < navLinks.length; i++) {
 
-    if (navLinks[i].getAttribute('href') == currentUrl) {
-        // Si la URL del enlace coincide con la URL actual de la página, agregamos la clase 'active'
-        navLinks[i].classList.add('active');
+  if (navLinks[i].getAttribute('href') == currentUrl) {
+    // Si la URL del enlace coincide con la URL actual de la página, agregamos la clase 'active'
+    navLinks[i].classList.add('active');
 
 
-    }
+  }
+}
+// ------------------------------------ Cards index admin ------------------------------------ //
+if (currentUrl == "/templates/admin/index_admin.html") {
+  const cards = document.querySelectorAll('.Lcard');
+
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+
+    card.addEventListener('mouseover', () => {
+      setTimeout(() => {
+        card.querySelector('.Ctexto').classList.toggle('display-none');
+        card.querySelector('.CtextoHover').classList.toggle('display-block');
+      }, 100);
+    });
+
+    card.addEventListener('mouseout', () => {
+      setTimeout(() => {
+        card.querySelector('.Ctexto').classList.toggle('display-none');
+        card.querySelector('.CtextoHover').classList.toggle('display-block');
+      }, 100);
+    });
+  }
+}
+// ------------------------------------ End Cards index admin ------------------------------------ //
+
+// ------------------------------------ Agregar practicante ------------------------------------ //
+if (currentUrl == "/templates/supervisor/agregar_practicante.html") {
+  console.log(currentUrl);
+  const editableInput = document.getElementById('input-editable');
+  const readonlyInput = document.getElementById('input-readonly');
+  const combinedInput = document.getElementById('correoPrac');
+
+  // Función para combinar los valores de los dos inputs
+  function combineInputs() {
+    const editableValue = editableInput.value;
+    const readonlyValue = readonlyInput.value;
+    combinedInput.value = `${editableValue}${readonlyValue}`;
+    console.log(combinedInput.value);
+  }
+
+  // Asignar el valor del input combinado antes de hacer el submit
+  document.getElementById('agregarPacticante').addEventListener('submit', function (event) {
+    combineInputs();
+  });
+
+  // Escuchar cambios en los dos inputs y actualizar el input combinado
+  editableInput.addEventListener('input', combineInputs);
+  readonlyInput.addEventListener('input', combineInputs);
+
+  const formulario = document.getElementById('agregarPacticante');
+
+  formulario.addEventListener('submit', function (event) {
+    event.preventDefault(); // Evita que se realice el submit por defecto del formulario
+  });
 }
 
+// ------------------------------------ End Agregar practicante ------------------------------------ //
 
-const fechaInput = document.getElementById('birthday');
-const horaInput = document.getElementById('time');
 
-// Añadir evento "change" al input de fecha
-fechaInput.addEventListener('change', () => {
-  const fechaSeleccionada = new Date(fechaInput.value);
-  const hoy = new Date();
-  
-  // Validar que la fecha seleccionada sea mayor o igual a la fecha actual
-  if (fechaSeleccionada < hoy) {
-    fechaInput.setCustomValidity('La fecha debe ser mayor o igual a la fecha actual');
-  } else {
-    fechaInput.setCustomValidity('');
+
+// ------------------------------------  loadMoreBtn ------------------------------------ //
+if (currentUrl == "/templates/paciente/agenda_cita.html") {
+  // Seleccionamos el botón y obtenemos las clases originales
+  const loadMoreBtn = document.querySelector('#loadMoreBtn');
+
+  // Función para intercambiar las clases del botón
+  function toggleClasses() {
+
+    if (window.innerWidth <= 993) {
+      if (window.innerWidth >= 723) {
+        loadMoreBtn.classList.remove('w-25');
+        loadMoreBtn.classList.add('w-sm-25');
+        loadMoreBtn.classList.remove('w-sm-100');
+      } else if (window.innerWidth < 723) {
+        loadMoreBtn.classList.add('w-sm-100');
+      }
+    } else if (window.innerWidth > 993) {
+      loadMoreBtn.classList.remove('w-sm-25');
+      loadMoreBtn.classList.add('w-25');
+    }
   }
-});
+  // Al cargar la página y al cambiar el tamaño de la pantalla, llamamos a la función toggleClasses
+  window.addEventListener('load', toggleClasses);
+  window.addEventListener('resize', toggleClasses);
+}
+// ------------------------------------ End loadMoreBtn ------------------------------------ //
 
-// Añadir evento "change" al input de hora
-horaInput.addEventListener('change', () => {
-  const horaSeleccionada = horaInput.value;
-  
-  // Obtener las horas y minutos de la hora seleccionada
-  const hora = parseInt(horaSeleccionada.substring(0, 2));
-  const minutos = parseInt(horaSeleccionada.substring(3));
-  
-  // Validar que la hora seleccionada sea entre las 8am y las 7pm
-  if (hora < 8 || hora > 19) {
-    horaInput.setCustomValidity('La hora seleccionada no es válida');
-    horaInput.classList.add('time-invalida');
-  } else {
-      horaInput.setCustomValidity('');
-    horaInput.classList.remove('time-invalida');
-  }
-});
-     
-//*********calendrio******** */
-const fechaHoraInput = document.getElementById("birthday-hora");
-const submitBtn = document.getElementById("submit-btn");
 
-fechaHoraInput.addEventListener("input", function() {
-  const fechaHoraSeleccionada = new Date(fechaHoraInput.value);
-  const horaSeleccionada = fechaHoraSeleccionada.getHours();
 
-  // Verificar si la hora seleccionada es válida
-  if (horaSeleccionada < 8 || horaSeleccionada > 19) {
-    // Hora inválida: deshabilitar botón de submit y cambiar color de fondo del input
-    submitBtn.disabled = true;
-    fechaHoraInput.style.backgroundColor = "red";
-  } else {
-    // Hora válida: habilitar botón de submit y restaurar color de fondo del input
-    submitBtn.disabled = false;
-    fechaHoraInput.style.backgroundColor = "white";
-  }
-});
+///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+///Hora y fecha
+
+// const fechaInput = document.getElementById('fecha');
+// const horaInput = document.getElementById('hora');
+
+// // Añadir evento "change" al input de fecha
+// fechaInput.addEventListener('change', () => {
+//   const fechaSeleccionada = new Date(fechaInput.value);
+//   const hoy = new Date();
+
+//   // Validar que la fecha seleccionada sea mayor o igual a la fecha actual
+//   if (fechaSeleccionada < hoy) {
+//     fechaInput.setCustomValidity('La fecha debe ser mayor o igual a la fecha actual');
+//   } else {
+//     fechaInput.setCustomValidity('');
+//   }
+// });
+
+// // Añadir evento "change" al input de hora
+// horaInput.addEventListener('change', () => {
+//   const horaSeleccionada = horaInput.value;
+
+//   // Obtener las horas y minutos de la hora seleccionada
+//   const hora = parseInt(horaSeleccionada.substring(0, 2));
+//   const minutos = parseInt(horaSeleccionada.substring(3));
+
+//   // Validar que la hora seleccionada sea entre las 8am y las 7pm
+//   if (hora < 8 || hora > 19) {
+//     horaInput.setCustomValidity('La hora seleccionada no es válida');
+//     horaInput.classList.add('hora-invalida');
+//   } else {
+//       horaInput.setCustomValidity('');
+//     horaInput.classList.remove('hora-invalida');
+//   }
+// });
+
+
+// const fechaHoraInput = document.getElementById("fecha-hora");
+// const submitBtn = document.getElementById("submit-btn");
+
+// fechaHoraInput.addEventListener("input", function() {
+//   const fechaHoraSeleccionada = new Date(fechaHoraInput.value);
+//   const horaSeleccionada = fechaHoraSeleccionada.getHours();
+
+//   // Verificar si la hora seleccionada es válida
+//   if (horaSeleccionada < 8 || horaSeleccionada > 19) {
+//     // Hora inválida: deshabilitar botón de submit y cambiar color de fondo del input
+//     submitBtn.disabled = true;
+//     fechaHoraInput.style.backgroundColor = "red";
+//   } else {
+//     // Hora válida: habilitar botón de submit y restaurar color de fondo del input
+//     submitBtn.disabled = false;
+//     fechaHoraInput.style.backgroundColor = "white";
+//   }
+// });
+///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+//    Tranferir datos a modal Administrador
+
+// Obtener la tabla y el modal
+// var table = document.getElementById("AdminTable");
+// var modal = document.getElementById("editEmployeeModal");
+
+// // Obtener los elementos del formulario
+// var nombreInput    = document.getElementById("nombreAd");
+// var apellidoPInput = document.getElementById("apellidoPAd");
+// var apellidoMInput = document.getElementById("apellidoMAd");
+// var correoInput    = document.getElementById("correoAd");
+
+// // Función para abrir el modal y llenar el formulario
+// function editar(button) {
+//   // Obtener la fila de la tabla
+//   var row = button.parentNode.parentNode;
+//   // Obtener los valores de las celdas
+//   var nombre    = row.cells[0].innerHTML;
+//   var apellidoP = row.cells[1].innerHTML;
+//   var apellidoM = row.cells[2].innerHTML;
+//   var correo    = row.cells[3].innerHTML;
+//   // Llenar el formulario con los valores de las celdas
+//   nombreInput.value      = nombre;
+//   apellidoPInput.value   = apellidoP;
+//   apellidoMInput.value   = apellidoM;
+//   correoInput.value      = correo;
+//   // Mostrar el modal
+//   modal.style.display = "block";
+// }
+
+// // Función para guardar los cambios
+// function guardar() {
+//   // Obtener los valores del formulario
+//   var nombre    = nombreInput.value;
+//   var apellidoP = apellidoPInput.value;
+//   var apellidoM = apellidoMInput.value;
+//   var correo    = correoInput.value;
+//   // Actualizar la fila de la tabla con los nuevos valores
+//   var row = table.getElementsByTagName("tr")[1];
+//   row.cells[0].innerHTML = nombre;
+//   row.cells[1].innerHTML = apellidoP;
+//   row.cells[2].innerHTML = apellidoM;
+//   row.cells[3].innerHTML = correo;
+//   // Cerrar el modal
+//   modal.style.display = "none";
+// }
+
+// // Función para cerrar el modal
+// var span = document.getElementsByClassName("btn-close")[0];
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+
+
+
+
+
+// var table = document.getElementById("AdminTable");
+// var form = document.getElementById("editAdmin");
+
+// // Agregar un evento "click" a cada fila de la tabla
+// for (var i = 0; i < table.rows.length; i++) {
+//   table.rows[i].addEventListener("click", function() {
+//     // Obtener los datos de la fila seleccionada
+//     var nombre = this.cells[0].innerHTML;
+//     var apellidoP = this.cells[1].innerHTML;
+//     var apellidoM = this.cells[2].innerHTML;
+//     var correo = this.cells[3].innerHTML;
+
+//     // Asignar los datos a los campos del formulario
+//     form.elements["nombreAd"].value    = nombre;
+//     form.elements["apellidoPAd"].value = apellidoP;
+//     form.elements["apellidoMAd"].value = apellidoM;
+//     form.elements["correoAd"].value    = correo;
+//   });
+// }
+
+// Obtener elementos de la tabla y del formulario
+// const tabla = document.getElementById("AdminTable");
+// const form = document.querySelector("#editEmployeeModal form");
+// const nombreInput = form.querySelector("#nombreAd");
+// const apellidoPInput = form.querySelector("#apellidoPAd");
+// const apellidoMInput = form.querySelector("#apellidoMAd");
+// const correoInput = form.querySelector("#correoAd");
+
+
+// // Función para mostrar el modal y transferir los datos de la fila correspondiente
+// function mostrarFormulario(boton) {
+//   // Obtener la fila correspondiente al botón presionado
+//   const fila = boton.closest("tr");
+
+//   // Obtener los datos de la fila y asignarlos a los campos del formulario
+//   const nombre = fila.querySelector("td:nth-child(1)").textContent;
+//   const apellidoP = fila.querySelector("td:nth-child(2)").textContent;
+//   const apellidoM = fila.querySelector("td:nth-child(3)").textContent;
+//   const correo = fila.querySelector("td:nth-child(4)").textContent;
+
+//   nombreInput.value = nombre;
+//   apellidoPInput.value = apellidoP;
+//   apellidoMInput.value = apellidoM;
+//   correoInput.value = correo;
+
+
+//   // Mostrar el modal
+//   const modal = document.getElementById("editEmployeeModal");
+//   modal.style.display = "block";
+// }
+
+// // Cerrar el modal cuando se envíe el formulario
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const modal = document.getElementById("editEmployeeModal");
+//   modal.style.display = "none";
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------------------- Fecha hora --------------------------- //
+
+if (currentUrl == "/templates/paciente/agenda_cita.html") {
+
+  // ---------------------------- Validacion fecha ---------------------------- //
+  // Obtenemos la fecha actual
+  var today = new Date().toISOString().split('T')[0];
+
+  // Obtenemos el elemento del input de fecha
+  var dateInput = document.getElementById('fecha');
+
+  // Establecemos la fecha mínima permitida como la fecha actual
+  dateInput.setAttribute('min', today);
+
+  // ---------------------------- End Validacion fecha ---------------------------- //
+
+  // ---------------------------- Validacion hora ---------------------------- //
+  const timeInput = document.getElementById('time');
+
+  timeInput.addEventListener('input', () => {
+    const time = timeInput.value;
+    const parts = time.split(':');
+    timeInput.value = `${parts[0]}:00`;
+  });
+
+  const timeError = document.getElementById('time-error');
+
+
+  timeInput.addEventListener('change', () => {
+    const selectedTime = timeInput.value;
+    const selectedHour = parseInt(selectedTime.split(':')[0], 10);
+
+    if (selectedHour < 8 || selectedHour > 20) {
+      timeInput.setCustomValidity('La hora debe estar entre las 8:00 y las 20:00');
+      timeInput.value = '';
+      timeError.style.display = 'block';
+
+      const optionElements = timeInput.querySelectorAll('option');
+      optionElements.forEach(option => {
+        const hour = parseInt(option.value.split(':')[0], 10);
+        if (hour < 8 || hour > 20) {
+          option.disabled = true;
+        } else {
+          option.disabled = false;
+        }
+      });
+    } else {
+      timeInput.setCustomValidity('');
+      timeError.style.display = 'none';
+
+      const optionElements = timeInput.querySelectorAll('option');
+      optionElements.forEach(option => {
+        option.disabled = false;
+      });
+    }
+  });
+
+  // Ver hora antes de enviar //
+  //   const form = document.getElementById('elegir')
+  //   form.addEventListener("submit", (event) => {
+  //     event.preventDefault();
+  //     console.log(timeInput.value ); 
+  //  });
+  // ---------------------------- End Validacion hora ---------------------------- //
+}
+// --------------------------- End Fecha hora --------------------------- //
